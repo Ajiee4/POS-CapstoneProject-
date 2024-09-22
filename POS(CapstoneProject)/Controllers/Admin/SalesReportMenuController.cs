e72e06d9e05582857f24757167d789e6998d4b44
Wiljen <loeljen982@gmail.com>
2024-09-22 17:40:35 +0800

    .

--- POS(CapstoneProject)/Controllers/Admin/SalesReportMenuController.cs
+++ POS(CapstoneProject)/Controllers/Admin/SalesReportMenuController.cs
@@ -59,160 +59,160 @@ namespace POS_CapstoneProject_.Controllers.Admin
             public int TotalStockIn { get; set; }
             public int TotalStockOut { get; set; }
         }
-        [HttpPost]
-        public async Task<IActionResult>   Index(string reportType, string filter)
-        {
-            if (reportType == "Sales")
-            {
+//        [HttpPost]
+//        public async Task<IActionResult>   Index(string reportType, string filter)
+//        {
+//            if (reportType == "Sales")
+//            {
 
-                if (filter == "Daily")
-                {
+//                if (filter == "Daily")
+//                {
 
 
-                    var Daily = _context.Order
-                        .GroupBy(o => o.OrderDate)
-                        .Select(g => new SalesReport
-                        {
-                            Date = g.Key,
-                            TotalSales = g.Sum(o => o.TotalAmount)
-                        })
-                        .OrderBy(g => g.Date)
-                        .ToList();
+//                    var Daily = _context.Order
+//                        .GroupBy(o => o.OrderDate)
+//                        .Select(g => new SalesReport
+//                        {
+//                            Date = g.Key,
+//                            TotalSales = g.Sum(o => o.TotalAmount)
+//                        })
+//                        .OrderBy(g => g.Date)
+//                        .ToList();
 
-                    var chartData = new
-                    {
-                        labels = Daily.Select(d => d.Date.ToString("MMMM dd, yyyy")),
-                        data = Daily.Select(d => d.TotalSales)
-                    };
-                    TempData["SalesFilter"] = "Daily Sales";
-                    ViewData["Sales"] = chartData;
+//                    var chartData = new
+//                    {
+//                        labels = Daily.Select(d => d.Date.ToString("MMMM dd, yyyy")),
+//                        data = Daily.Select(d => d.TotalSales)
+//                    };
+//                    TempData["SalesFilter"] = "Daily Sales";
+//                    ViewData["Sales"] = chartData;
                   
-                }
-                else if (filter == "Monthly")
-                {
+//                }
+//                else if (filter == "Monthly")
+//                {
 
-                    var orders = _context.Order.ToList();
+//                    var orders = _context.Order.ToList();
 
-                    var monthlySalesData = orders
-                        .GroupBy(o => new { Year = o.OrderDate.Year, Month = o.OrderDate.Month })
-                        .Select(g => new SalesReport
-                        {
-                            Date = new DateTime(g.Key.Year, g.Key.Month, 1),
-                            TotalSales = g.Sum(o => o.TotalAmount)
-                        })
-                        .OrderBy(g => g.Date)
-                        .ToList();
+//                    var monthlySalesData = orders
+//                        .GroupBy(o => new { Year = o.OrderDate.Year, Month = o.OrderDate.Month })
+//                        .Select(g => new SalesReport
+//                        {
+//                            Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+//                            TotalSales = g.Sum(o => o.TotalAmount)
+//                        })
+//                        .OrderBy(g => g.Date)
+//                        .ToList();
 
-                    var chartData = new
-                    {
-                        labels = monthlySalesData.Select(d => d.Date.ToString("MMMM yyyy")),
-                        data = monthlySalesData.Select(d => d.TotalSales)
-                    };
-                    TempData["SalesFilter"] = "Monthly Sales";
-                    ViewData["Sales"] = chartData;
+//                    var chartData = new
+//                    {
+//                        labels = monthlySalesData.Select(d => d.Date.ToString("MMMM yyyy")),
+//                        data = monthlySalesData.Select(d => d.TotalSales)
+//                    };
+//                    TempData["SalesFilter"] = "Monthly Sales";
+//                    ViewData["Sales"] = chartData;
                   
-                }
-                else if (filter == "Yearly")
-                {
-                    var orders = _context.Order.ToList();
+//                }
+//                else if (filter == "Yearly")
+//                {
+//                    var orders = _context.Order.ToList();
 
-                    var yearlySalesData = orders
-                        .GroupBy(o => o.OrderDate.Year)
-                        .Select(g => new SalesReport
-                        {
-                            Date = new DateTime(g.Key, 1, 1),
-                            TotalSales = g.Sum(o => o.TotalAmount)
-                        })
-                        .OrderBy(g => g.Date)
-                        .ToList();
+//                    var yearlySalesData = orders
+//                        .GroupBy(o => o.OrderDate.Year)
+//                        .Select(g => new SalesReport
+//                        {
+//                            Date = new DateTime(g.Key, 1, 1),
+//                            TotalSales = g.Sum(o => o.TotalAmount)
+//                        })
+//                        .OrderBy(g => g.Date)
+//                        .ToList();
 
-                    var chartData = new
-                    {
-                        labels = yearlySalesData.Select(d => d.Date.ToString("yyyy")),
-                        data = yearlySalesData.Select(d => d.TotalSales)
-                    };
-                    TempData["SalesFilter"] = "Yearly Sales";
-                    ViewData["Sales"] = chartData;
+//                    var chartData = new
+//                    {
+//                        labels = yearlySalesData.Select(d => d.Date.ToString("yyyy")),
+//                        data = yearlySalesData.Select(d => d.TotalSales)
+//                    };
+//                    TempData["SalesFilter"] = "Yearly Sales";
+//                    ViewData["Sales"] = chartData;
                    
-                }
+//                }
 
-            }
+//            }
 
-            else if (reportType == "Inventory")
-            {
-                if (filter == "Daily")
-                {
-                    var Daily = await (from it in _context.InventoryTransaction
-                                join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId  into gj
-                                       from sub in gj.DefaultIfEmpty()
-                                group new { it, sub } by it.TransactionDate into g
-                                select new InventoryReport
-                                {
-                                    Date = g.Key,
-                                    TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
-                                    TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0),
+//            else if (reportType == "Inventory")
+//            {
+//                if (filter == "Daily")
+//                {
+//                    var Daily = await (from it in _context.InventoryTransaction
+//                                join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId  into gj
+//                                       from sub in gj.DefaultIfEmpty()
+//                                group new { it, sub } by it.TransactionDate into g
+//                                select new InventoryReport
+//                                {
+//                                    Date = g.Key,
+//                                    TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
+//                                    TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0),
                                    
-                                }).ToListAsync();
+//                                }).ToListAsync();
 
-                    var chartData = new ChartData
-                    {
-                        Labels = Daily.Select(d => d.Date.ToString("MMMM dd, yyyy")).ToArray(),
-                        Data = Daily.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
-                    };
+//                    var chartData = new ChartData
+//                    {
+//                        Labels = Daily.Select(d => d.Date.ToString("MMMM dd, yyyy")).ToArray(),
+//                        Data = Daily.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
+//                    };
 
-                    TempData["SalesFilter"] = "Daily Stock";
-                    ViewData["Inventory"] = chartData;
-                }
-                else if(filter == "Monthly")
-{
-                    var Monthly = await (from it in _context.InventoryTransaction
-                                         join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId into gj
-                                         from sub in gj.DefaultIfEmpty()
-                                         group new { it, sub } by new { Year = it.TransactionDate.Year, Month = it.TransactionDate.Month } into g
-                                         select new
-                                         {
-                                             Date = new DateTime(g.Key.Year, g.Key.Month, 1),
-                                             TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
-                                             TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0)
-                                         }).ToListAsync();
+//                    TempData["SalesFilter"] = "Daily Stock";
+//                    ViewData["Inventory"] = chartData;
+//                }
+//                else if(filter == "Monthly")
+//{
+//                    var Monthly = await (from it in _context.InventoryTransaction
+//                                         join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId into gj
+//                                         from sub in gj.DefaultIfEmpty()
+//                                         group new { it, sub } by new { Year = it.TransactionDate.Year, Month = it.TransactionDate.Month } into g
+//                                         select new
+//                                         {
+//                                             Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+//                                             TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
+//                                             TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0)
+//                                         }).ToListAsync();
 
-                    var chartData = new ChartData
-                    {
-                        Labels = Monthly.Select(d => d.Date.ToString("MMMM yyyy")).ToArray(),
-                        Data = Monthly.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
-                    };
+//                    var chartData = new ChartData
+//                    {
+//                        Labels = Monthly.Select(d => d.Date.ToString("MMMM yyyy")).ToArray(),
+//                        Data = Monthly.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
+//                    };
 
-                    TempData["SalesFilter"] = "Monthly Stock";
-                    ViewData["Inventory"] = chartData;
-                }
-                else if (filter == "Yearly")
-                {
-                    var Yearly = await (from it in _context.InventoryTransaction
-                                 join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId into gj
-                                 from sub in gj.DefaultIfEmpty()
-                                 group new { it, sub } by it.TransactionDate.Year into g
-                                 select new 
-                                 {
-                                     Date = new DateTime(g.Key, 1, 1), // First day of the year
-                                     TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
-                                     TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0)
-                                 }).ToListAsync();
+//                    TempData["SalesFilter"] = "Monthly Stock";
+//                    ViewData["Inventory"] = chartData;
+//                }
+//                else if (filter == "Yearly")
+//                {
+//                    var Yearly = await (from it in _context.InventoryTransaction
+//                                 join itd in _context.InventoryTransactionDetail on it.InventoryTransactId equals itd.InventoryTransactId into gj
+//                                 from sub in gj.DefaultIfEmpty()
+//                                 group new { it, sub } by it.TransactionDate.Year into g
+//                                 select new 
+//                                 {
+//                                     Date = new DateTime(g.Key, 1, 1), // First day of the year
+//                                     TotalStockIn = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock In" ? x.sub.Quantity : 0),
+//                                     TotalStockOut = g.Sum(x => x.sub != null && x.it.TransactionType == "Stock Out" ? x.sub.Quantity : 0)
+//                                 }).ToListAsync();
                   
 
-                    var chartData = new ChartData
-                    {
-                        Labels = Yearly.Select(d => d.Date.ToString("yyyy")).ToArray(), // Display year only
-                        Data = Yearly.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
-                    };
+//                    var chartData = new ChartData
+//                    {
+//                        Labels = Yearly.Select(d => d.Date.ToString("yyyy")).ToArray(), // Display year only
+//                        Data = Yearly.Select(d => new ChartDataPoint { TotalStockIn = d.TotalStockIn, TotalStockOut = d.TotalStockOut }).ToArray()
+//                    };
 
-                    TempData["SalesFilter"] = "Yearly Stock";
-                    ViewData["Inventory"] = chartData;
-                }
+//                    TempData["SalesFilter"] = "Yearly Stock";
+//                    ViewData["Inventory"] = chartData;
+//                }
 
-            }
+//            }
 
-            return View();
-        }
+//            return View();
+//        }
 
         [HttpPost]
         public async Task< IActionResult > GetChartData(string type, DateTime fromDate, DateTime toDate)
